98b1a3dc1157a1afeb8a71721310d8d22185c151
karlmorand <karljmorand@gmail.com>
2017-01-16 21:11:11 -0500

    users can now sign up with their name and email, which is added to the db and shown on the users index page

--- app/routes/users/new.js
+++ app/routes/users/new.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+  return this.store.createRecord('user')
+},
+
+  actions:{
+    createUser(newUser){
+      newUser.save().then(()=>this.transitionTo('users.index'))
+    }
+  }
 });
